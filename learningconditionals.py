06cf4c60c15af3d4d26ba4d556b646dbecdcca98
ignw <ignw@ignw-jumphost.ignw.io>
2019-09-18 15:12:01 +0000

    Adding Lab Pyton files

---
+++ learningconditionals.py
@@ -0,0 +1,13 @@
+my_integer = 443
+if my_integer > 0:
+    print ("Hey, that looks like a positive number!")
+new_string = "You call that a string"
+if "?" in new_string:
+    print ("Yep, there is ? in that string")
+elif ":" in new_string:
+    print ("Yep, there is a colon...")
+elif "string" in new_string:
+    print ("Yep, the word string is in there...")
+else:
+    print ("Whoa, we got a catch-all now!")
+    
